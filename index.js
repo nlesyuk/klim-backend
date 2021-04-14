7e4fb048fb986f5efc89b6956786f7e41bea0cc5
Nazar Lesyuk <pronaza@gmail.com>
2021-04-14 21:04:17 +0300

    Processing dynamit routes is done

--- index.js
+++ index.js
@@ -14,8 +14,26 @@ const server = http.createServer((req, res) => {
         'Access-Control-Max-Age': 2592000, // 30 days
     };
     console.log('\x1b[33m%s\x1b[0m', 'Request URL: ', "\x1b[31m", req.url);
+    let reqUrl = req.url
+    let postId
 
-    switch (req.url) {
+    const videoPost = req.url.match(/videos\/\d+/)
+    const correctPathVideo = videoPost ? videoPost[0] : videoPost
+    if (correctPathVideo) {
+        reqUrl = 'videoPost'
+        postId = `${correctPathVideo}`.split('/')[1]
+        console.log(videoPost ? videoPost[0] : videoPost)
+    }
+
+    const photoPost = req.url.match(/photos\/\d+/)
+    const correctPathPhoto = photoPost ? photoPost[0] : photoPost
+    if (correctPathPhoto) {
+        reqUrl = 'photoPost'
+        postId = `${correctPathPhoto}`.split('/')[1]
+        console.log(photoPost ? photoPost[0] : photoPost)
+    }
+
+    switch (reqUrl) {
         case '/videos':
             fs.readFile(config.pathToTempDB, (error, data) => {
                 if (error) throw error
@@ -53,6 +71,30 @@ const server = http.createServer((req, res) => {
 
                 db = JSON.parse(data)
                 res.write(JSON.stringify(db.contact))
+                res.end()
+            })
+            break;
+        case 'videoPost':
+            fs.readFile(config.pathToTempDB, (error, data) => {
+                if (error) throw error
+                res.writeHead(200, headers);
+
+                db = JSON.parse(data)
+                let post = db.videos.filter(v => +v.id === +postId)
+                post = post.length ? post[0] : null
+                res.write(JSON.stringify(post))
+                res.end()
+            })
+            break;
+        case 'photoPost':
+            fs.readFile(config.pathToTempDB, (error, data) => {
+                if (error) throw error
+                res.writeHead(200, headers);
+
+                db = JSON.parse(data)
+                let post = db.photos.filter(v => +v.id === +postId)
+                post = post.length ? post[0] : null
+                res.write(JSON.stringify(post))
                 res.end()
             })
             break;
