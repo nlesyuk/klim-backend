0a52ef6291afad77a1047be5ab8205a005ede5d7
Nazar Lesyuk <pronaza@gmail.com>
2021-12-04 22:17:05 +0200

    Slider: add mock data

--- controllers/slider.controller.js
+++ controllers/slider.controller.js
@@ -15,14 +15,73 @@ class SliderController {
   async get(req, res) {
     const d = getCurrentDateTime()
     console.log('------------------------------------getSlider-START', d)
-    res.status(200).json({ message: 'get' })
+    const slides = [
+      {
+        type: "video",
+        workId: "7",
+        video: {
+          vimeoId: 561200941
+        }
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/2.jpg",
+        workId: "4"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/11.jpg",
+        workId: "4"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/1.jpg",
+        photoId: "4"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/12.jpg",
+        workId: "4"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/3.jpg",
+        workId: "1"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/14.jpg",
+        workId: "4"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/15.jpg",
+        workId: "4"
+      },
+      {
+        type: "image",
+        image: "https://nrdevux.com/klim/slides/16.jpg",
+        workId: "4"
+      },
+      {
+        type: "video",
+        workId: "9",
+        video: {
+          vimeoId: 561200941
+        }
+      }
+    ]
+
+    res.status(200).json(slides)
     console.log('------------------------------------getSlider-END', d)
   }
 
   async getById(req, res) {
     const d = getCurrentDateTime()
     console.log('------------------------------------getSliderById-START', d)
-    res.status(200).json({ message: 'getById' })
+
+
+    res.status(200).json({ message: 'get' })
     console.log('------------------------------------getSliderById-END', d)
   }
 
